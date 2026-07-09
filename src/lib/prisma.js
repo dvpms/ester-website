25af8e94ca86e0b8b9abe2ce7d3df4afaad66630
Devran Perdana Malik <devranmalik82@gmail.com>
2026-07-09 12:10:12 +0700

    feat: initialize Prisma setup with PostgreSQL datasource and User model
    - Added prisma.config.ts for Prisma configuration.
    - Created initial migration for User table with email uniqueness.
    - Added migration lock file for version control.
    - Defined Prisma schema with User model including fields: id, name, email, and createdAt.
    - Implemented Prisma client setup in src/lib/prisma.js with PostgreSQL adapter.

---
+++ src/lib/prisma.js
@@ -0,0 +1,13 @@
+import { PrismaClient } from "../../prisma/generated/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+import "dotenv/config";
+
+const globalForPrisma = globalThis;
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+const adapter = new PrismaPg(pool);
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
